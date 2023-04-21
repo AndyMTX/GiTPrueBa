310922baf91d18e0c5b81582221af167465c9bd2
aemlocal <aemlocal@ED49005994P022.educa.jcyl.es>
2023-04-21 12:44:57 +0200

    scanner añadido

--- JavaGit/src/paquete/NumMayorApp.java
+++ JavaGit/src/paquete/NumMayorApp.java
@@ -1,11 +1,14 @@
 package paquete;
 
+import java.util.Scanner;
+
 public class NumMayorApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Añadid un scanner sin más Scanner sc=new Scanner(System.in);
+		//Añadid un scanner sin más 
+		Scanner sc=new Scanner(System.in);
 		//Para que metáis algo en el proyecto de verdad
 		int a, b;
 		NumMayor compara=new NumMayor();
