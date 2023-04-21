99d5032de1719194c2a63b10276e5cfc8b60e328
aemlocal <aemlocal@ED49005994P024.educa.jcyl.es>
2023-04-21 12:41:14 +0200

    Cositas

---
+++ JavaGit/src/paquete/NumMayor.java
@@ -0,0 +1,20 @@
+package paquete;
+
+public class NumMayor {
+
+	public boolean isA(int a, int b) {
+		if (a>b){
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public boolean isB(int a, int b) {
+		if (b>a){
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
---
+++ JavaGit/src/paquete/NumMayorApp.java
@@ -0,0 +1,23 @@
+package paquete;
+
+public class NumMayorApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Añadid un scanner sin más Scanner sc=new Scanner(System.in);
+		//Para que metáis algo en el proyecto de verdad
+		int a, b;
+		NumMayor compara=new NumMayor();
+		
+		//Aqui que otro añada el outprint de introducir a y b
+		if(compara.isA(a, b)) {
+			System.out.println(a + " es mayor que "+ b);
+		}else if(compara.isB(a, b)){
+			System.out.println(b + " es mayor que "+ a);
+		}else {
+			System.out.println("Ambos números son iguales");
+		}
+	}
+
+}
